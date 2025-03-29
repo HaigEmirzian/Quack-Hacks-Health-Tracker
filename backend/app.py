df6fd6ca0b0f020d3d7e9dc842f065eecb6cf5b5
Brian Sampson <sampsonbrian862@gmail.com>
2025-03-29 15:55:52 -0400

    Added apple upload data endpoint

--- backend/app.py
+++ backend/app.py
@@ -44,5 +44,33 @@ def prediction():
         return jsonify({"error": f"Error during prediction: {str(e)}"})
 
 
+UPLOAD_FOLDER = "/appleHealth/"
+
+@app.route("/appleDataUpload", methods=["POST"])
+def appleDataUpload():
+    file = request.files.get("file")
+
+    # Validate file
+    if file is None or file.filename == "":
+        return jsonify({"error": "No file uploaded"}), 400
+    if not file.filename.endswith('.xml'):
+        return jsonify({"error": "File type not allowed. Please upload an XML file."}), 400
+
+    # Define the fixed filename
+    fixed_filename = "uploadData.xml"
+    file_path = os.path.join(UPLOAD_FOLDER, fixed_filename)
+
+    # Save the file to the upload folder with the fixed filename
+    try:
+        file.save(file_path)
+        return jsonify({"message": f"File uploaded successfully as '{fixed_filename}' to '{file_path}'."}), 200
+    except Exception as e:
+        return jsonify({"error": f"Error saving file: {str(e)}"}), 500
+    
+
+@app.route("/heartbeat", methods=["GET"])
+def heartbeat():
+    return jsonify({"message": "Success!"})
+
 if __name__ == "__main__":
     app.run(debug=True)
